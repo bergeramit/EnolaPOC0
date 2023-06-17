392f800901313e1dc5284463edae0bd8451a03e7
Amit Berger <bergeramit@gmail.com>
2023-06-17 19:03:35 +0300

    added bot mechanism

--- src/public/js/test_levels.js
+++ src/public/js/test_levels.js
@@ -1,21 +1,17 @@
-var postURL = "http://64.226.100.123/generate_level/";
-//const postURL = 'http://127.0.0.1:3000/generate_level/'
+//var postURL = "http://64.226.100.123/generate_level/";
+const postURL = 'http://127.0.0.1:3000/generate_level/'
 let difficulty = 'Easy'
 let levelDataStructure
+let metaLevelDataStructure
 let levelNumber = 0
 let timeoutBetweenLevels = 1000;
 let correctlyGuessed = {}
 let availableLetters
 let enterKeyName = "Enter"
+let spaceKeyName = "space"
 let backspaceKeyName = "Backspace"
-
-function appendMessage (username, message) {
-    const chatMessages = document.getElementById('chat-messages')
-    const messageElement = document.createElement('div')
-    messageElement.textContent = username + ': ' + message
-    chatMessages.appendChild(messageElement)
-    messageElement.scrollIntoView()
-}
+let botUserNames = ["user12431", "smartFox69", "Huberman32", "WordyJack3"]
+let botMessages = ["a though one!", "Damn", "So close!", "almost almost", "thats so fun"]
 
 function countLetter (letter, str) {
     let letterCount = 0
@@ -30,6 +26,159 @@ function countLetter (letter, str) {
     return letterCount
 }
 
+
+function checkGuess (guess) {
+    // console.log("in CheckGuess");
+    for (let i = 0; i < levelDataStructure.length; i++) {
+        //console.log('Checks: levelDataStructure[1][i]: ' + levelDataStructure[i] + ' === ' + guess)
+        if (levelDataStructure[i] === guess && !correctlyGuessed[i]) {
+            correctlyGuessed[i] = true
+            //console.log('Success! at row: ' + (i + 1))
+            const row = document.getElementsByClassName('letter-local')[i]
+            for (let j = 0; j < row.children.length; j++) {
+                // place word correctly!
+                row.children[j].textContent = levelDataStructure[i][j]
+            }
+            
+            for (const key of Object.keys(correctlyGuessed)) {
+                if (!correctlyGuessed[key]) {
+                    // There are still empty rows
+                    return true
+                }
+            }
+            // generate new level
+            appendMessage('WordHunt', 'Great job!')
+            appendMessage('WordHunt', 'Get Ready for level ' + (levelNumber+1))
+            setTimeout(() => {
+                generateNewLevel()
+            }, timeoutBetweenLevels)
+            return true
+        }
+    }
+
+    return false
+}
+
+/* ---------------------- User Inputs ---------------------- */
+
+
+function addKeyToInput (pressedKey, onScreen) {
+    const guess = document.getElementById('input-guess')
+    if (pressedKey === backspaceKeyName && onScreen) {
+        guess.value = guess.value.substring(0, guess.value.length - 1)
+        return
+    }
+
+    if (pressedKey === spaceKeyName && onScreen) {
+        guess.value = guess.value + " "
+        return
+    }
+    
+    if (pressedKey === enterKeyName) {
+        if (!checkGuess(guess.value)) {
+            // add to chat instead
+            appendMessage('you', guess)
+        }else {
+            appendMessage("WordHunt", "you solved a row!")
+        }
+        guess.value = ''
+        return
+    }
+
+    if (onScreen) {
+        guess.value = guess.value + pressedKey.toLowerCase()[0]
+    }
+}
+
+document.getElementById('keyboard-cont').addEventListener('click', (e) => {
+    const target = e.target
+    if (!target.classList.contains('keyboard-button')) {
+        return
+    }
+    
+    let key = target.textContent
+    const guess = document.getElementById('input-guess')
+    if (target.className.includes("fa-share")) {
+        if (!checkGuess(guess.value)) {
+            // add to chat instead
+            appendMessage('you', guess.value)
+        } else {
+            appendMessage("WordHunt", "you solved a row!")
+        }
+        guess.value = ''
+    }
+    else if (target.className.includes("backspace")) {
+        guess.value = guess.value.substring(0, guess.value.length - 1)
+        return
+    }
+    else if (key == spaceKeyName) {
+        guess.value = guess.value + " "
+        return
+    } else {
+        guess.value = guess.value + key.toLowerCase()[0]
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    const pressedKey = String(e.key)
+    addKeyToInput(pressedKey, false) // actual press
+})
+
+const buttons = document.querySelectorAll('.pagination a')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttons.forEach((btn) => {
+            btn.classList.remove('active')
+        })
+        
+        button.classList.add('active')
+        difficulty = button.textContent
+        console.log(difficulty)
+    })
+})
+
+/* ---------------------- /User Inputs ---------------------- */
+
+/* ---------------------- Server API ---------------------- */
+function generateNewLevel () {
+    correctlyGuessed = {}
+    const levelNumberObj = document.getElementById('level-number')
+    
+    levelNumber += 1
+    levelNumberObj.textContent = 'Level: ' + levelNumber
+
+    fetch(postURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({ difficulty })
+    }).then(response => {
+        console.log(response.statusText)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        metaLevelDataStructure = data.metaLevel
+        paintCurrentLevel(data.level)
+    })
+}
+/* ---------------------- /Server API ---------------------- */
+
+/* ---------------------- Chat ---------------------- */
+function appendMessage (username, message) {
+    const chatMessages = document.getElementById('chat-messages')
+    const messageElement = document.createElement('div')
+    messageElement.textContent = username + ': ' + message
+    chatMessages.appendChild(messageElement)
+    messageElement.scrollIntoView()
+}
+/* ---------------------- /Chat ---------------------- */
+
+/* ---------------------- Level Generation ---------------------- */
 function paintCurrentLevel (currentLevel) {
     levelDataStructure = currentLevel
     availableLetters = Array.from(levelDataStructure[0])
@@ -67,137 +216,27 @@ function paintCurrentLevel (currentLevel) {
     })
 }
 
-function checkGuess (guess) {
-    // console.log("in CheckGuess");
-    for (let i = 0; i < levelDataStructure.length; i++) {
-        console.log('Checks: levelDataStructure[1][i]: ' + levelDataStructure[i] + ' == ' + guess)
-        if (levelDataStructure[i] === guess && !correctlyGuessed[i]) {
-            correctlyGuessed[i] = true
-            console.log('Sucess! at row: ' + i + 1)
-            const row = document.getElementsByClassName('letter-local')[i]
-            for (let j = 0; j < row.children.length; j++) {
-                // place word correctly!
-                row.children[j].textContent = levelDataStructure[i][j]
-            }
-            
-            for (const key of Object.keys(correctlyGuessed)) {
-                if (!correctlyGuessed[key]) {
-                    // There are still empty rows
-                    return
-                }
-            }
-            // generate new level
-            appendMessage('WordHunt', 'Great job!')
-            appendMessage('WordHunt', 'Get Ready for level ' + (levelNumber+1))
-            setTimeout(() => {
-                geneateNewLevel()
-            }, timeoutBetweenLevels)
-            return
-        }
-    }
-    // add to chat instead
-    appendMessage('you', guess)
-    // toastr.error("Wrong Guess!"); -> can remove toast from requires
-}
-
-function addKeyToInput (pressedKey, onScreen) {
-    const guess = document.getElementById('input-guess')
-    if (pressedKey === backspaceKeyName) {
-        if (onScreen) {
-            guess.value = guess.value.substring(0, guess.value.length - 1)
-        }
-        return
-    }
-    
-    if (pressedKey === enterKeyName) {
-        checkGuess(guess.value)
-        // add to chat if not a correct guess
-        guess.value = ''
-        return
-    }
-    if (onScreen) {
-        guess.value = guess.value + pressedKey.toLowerCase()[0]
-    }
-}
-
-function geneateNewLevel () {
-    correctlyGuessed = {}
-    const levelNumberObj = document.getElementById('level-number')
-    
-    levelNumber += 1
-    levelNumberObj.textContent = 'Level: ' + levelNumber
-
-    fetch(postURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
-        },
-        body: JSON.stringify({ difficulty })
-    }).then(response => {
-        console.log(response.statusText)
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        paintCurrentLevel(data)
-    })
-}
-
-/* ---------------------- Listeners ---------------------- */
-
-document.getElementById('keyboard-cont').addEventListener('click', (e) => {
-    const target = e.target
-    
-    if (!target.classList.contains('keyboard-button')) {
-        return
-    }
-    
-    let key = target.textContent
-    if (key === 'Generate Level') {
-        geneateNewLevel()
-        return
-    }
-    if (target.className.includes("fa-share")) {
-        key = enterKeyName
-    }
-    if (target.className.includes("backspace")) {
-        key = backspaceKeyName
-    }
-    addKeyToInput(key, true) // onscreen press
-})
-
-document.addEventListener('keyup', (e) => {
-    const pressedKey = String(e.key)
-    addKeyToInput(pressedKey, false) // actual press
-})
-
-const buttons = document.querySelectorAll('.pagination a')
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        buttons.forEach((btn) => {
-            btn.classList.remove('active')
-        })
-        
-        button.classList.add('active')
-        difficulty = button.textContent
-        console.log(difficulty)
-    })
-})
-
-/* ---------------------- /Listeners ---------------------- */
-
-/* ---------------------- Listeners ---------------------- */
-/* ---------------------- Listeners ---------------------- */
-/* ---------------------- Listeners ---------------------- */
-
-/* ---------------------- Startup ---------------------- */
 function setupStartingLevel () {
-    geneateNewLevel()
+    generateNewLevel()
     appendMessage('WordHunt', 'Welcome!')
     appendMessage('WordHunt', 'Try and guess the words and communicate with your friends!')
+    setInterval(runBotGuesser, 5000);
 }
 setupStartingLevel()
-/* ---------------------- /Startup ---------------------- */
+/* ---------------------- /Level Generation ---------------------- */
+
+
+/* ---------------------- /Bot Logic ---------------------- */
+function runBotGuesser() {
+
+    let botUserName = botUserNames[Math.floor(Math.random()*botUserNames.length)]
+    //let botMessage = botMessages[Math.floor(Math.random()*botMessages.length)]
+    let botGuess = metaLevelDataStructure[Math.floor(Math.random()*metaLevelDataStructure.length)]
+    if (checkGuess(botGuess)) {
+        appendMessage("WordHunt", botUserName + " solved a row!")
+    } else {
+        appendMessage(botUserName, botGuess)
+    }
+}
+
+/* ---------------------- /Bot Logic ---------------------- */
--- src/server/gameLogic/test_levels.service.js
+++ src/server/gameLogic/test_levels.service.js
@@ -1,30 +1,42 @@
 const fs = require('fs');
 
 var levels;
+var metaLevels;
+var currentMetaLevel;
 
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max-min)) + min;
-  }
+}
 
-  function compareFn(a, b) {
+function compareFn(a, b) {
     if (a[0] < b[0]) {
-      return -1;
+        return -1;
     }
     if (a[0] > b[0]) {
-      return 1;
+        return 1;
     }
     return 0;
-  }
+}
 
 fs.readFile("../gameLogic/resources/easy_levels_dataset.json", "utf8", (err, jsonString) => {
     if (err) {
-    console.log("File read failed:", err);
-    return;
+        console.log("File read failed:", err);
+        return;
     }
     levels = JSON.parse(jsonString);
     levels.sort(compareFn);
     console.log(levels.length);
     console.log(levels[levels.length-1]);
+});
+
+fs.readFile("../gameLogic/resources/easy_meta_levels.json", "utf8", (err, jsonString) => {
+    if (err) {
+        console.log("File read failed:", err);
+        return;
+    }
+    metaLevels = JSON.parse(jsonString);
+    console.log(metaLevels.length);
+    console.log(metaLevels[metaLevels.length-1]);
 });
 
 function retrieve_level(difficulty) {
@@ -35,15 +47,18 @@ function retrieve_level(difficulty) {
         "Very Hard": [Math.floor(3 * levels.length / 4), levels.length-1],
     }
     difficulty_range = diff_to_range[difficulty]
-    console.log(difficulty_range);
     var lower = difficulty_range[0];
     var upper = difficulty_range[1];
-    console.log(lower);
-    console.log(upper);
     var level_difficulty = getRandomArbitrary(lower, upper);
-    console.log(level_difficulty)
-
-    return levels[level_difficulty][1];
+    
+    for (let i=0;i<metaLevels.length;i++) {
+        console.log(metaLevels[i][0]);
+        if (metaLevels[i][0] == levels[level_difficulty][1][0]) {
+            currentMetaLevel = metaLevels[i]
+        }
+    }
+    
+    return {"level": levels[level_difficulty][1], "metaLevel": currentMetaLevel};
 }
 
 module.exports.retrieve_level = retrieve_level;
