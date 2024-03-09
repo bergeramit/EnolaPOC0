9b1dd3b82a3ee74ac87eda60b5f5a111591ac226
amit <amit@glue.games>
2024-03-09 11:53:27 +0200

    shrare pic

--- src/public/js/word_hunt.js
+++ src/public/js/word_hunt.js
@@ -241,7 +241,7 @@ function processWrongGuess() {
 
 function shareWhatsapp() {
     // Sharing to whatsapp instead
-    window.open('whatsapp://send?text='+encodeURIComponent("img/LevelsBackground/bg.png")+" I solved the challenge with no help! how about you");
+    window.open('whatsapp://send?text='+encodeURIComponent("https://picwiz.ai/src/public/img/LevelsBackground/bg.png")+" I solved the challenge with no help! how about you");
 }
 
 async function share() {
